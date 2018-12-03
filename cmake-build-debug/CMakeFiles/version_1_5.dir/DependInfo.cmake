be4d6833a66d3f0aa0b3f1179bd73380eb73a8a9
Githubwyb <1061322005@qq.com>
2018-12-03 09:57:39 +0800

    Commit version 1.5.0

---
+++ cmake-build-debug/CMakeFiles/version_1_5.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/work/seedclass/svn/Game_Monopoly/version_1.5/cmd.c" "E:/work/seedclass/svn/Game_Monopoly/version_1.5/cmake-build-debug/CMakeFiles/version_1_5.dir/cmd.c.obj"
+  "E:/work/seedclass/svn/Game_Monopoly/version_1.5/drawMap.c" "E:/work/seedclass/svn/Game_Monopoly/version_1.5/cmake-build-debug/CMakeFiles/version_1_5.dir/drawMap.c.obj"
+  "E:/work/seedclass/svn/Game_Monopoly/version_1.5/events.c" "E:/work/seedclass/svn/Game_Monopoly/version_1.5/cmake-build-debug/CMakeFiles/version_1_5.dir/events.c.obj"
+  "E:/work/seedclass/svn/Game_Monopoly/version_1.5/game.c" "E:/work/seedclass/svn/Game_Monopoly/version_1.5/cmake-build-debug/CMakeFiles/version_1_5.dir/game.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/version_1_5.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/version_1_5.dir/cmd.c.obj"
+  "CMakeFiles/version_1_5.dir/drawMap.c.obj"
+  "CMakeFiles/version_1_5.dir/events.c.obj"
+  "CMakeFiles/version_1_5.dir/game.c.obj"
+  "version_1_5.pdb"
+  "version_1_5.exe"
+  "version_1_5.exe.manifest"
+  "libversion_1_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/version_1_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
